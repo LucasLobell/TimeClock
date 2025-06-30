3c5033f52b0193dc52d9c4b11d17beecb5214da5
Lucas <-g>
2025-06-29 23:36:01 -0300

    test

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -10,7 +10,6 @@ const page = () => {
   const [loggedInUser, setLoggedInUser] = useState<any>(null);
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
-  // Fetch user ID on mount
   useEffect(() => {
     account.get().then(user => {
       setUserId(user.$id);
